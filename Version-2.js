d9a5ce69a58a5732daf24068c7d5120936d7fd3c
West7014 <114443603+West7014@users.noreply.github.com>
2023-06-07 23:31:42 -0600

    Create Version-2.js

---
+++ Version-2.js
@@ -0,0 +1,123 @@
+// Register file
+class RegisterFile {
+  constructor() {
+    this.reg = new Array(8).fill(0);
+  }
+}
+
+// Memory
+class Memory {
+  constructor() {
+    this.data = new Array(1024).fill(0);
+  }
+
+  store(address, value) {
+    if (address >= 0 && address < this.data.length) {
+      this.data[address] = value;
+    } else {
+      console.log("Error: Invalid memory address!");
+    }
+  }
+
+  load(address) {
+    if (address >= 0 && address < this.data.length) {
+      return this.data[address];
+    } else {
+      console.log("Error: Invalid memory address!");
+      return 0;
+    }
+  }
+}
+
+// CPU
+class CPU {
+  constructor() {
+    this.programCounter = 0;
+    this.regFile = new RegisterFile();
+    this.memory = new Memory();
+  }
+
+  executeCommand(command) {
+    const parts = command.split(" ");
+
+    switch (parts[0]) {
+      case "add": {
+        const register = parseInt(parts[1]);
+        const value = parseInt(parts[2]);
+
+        if (register >= 1 && register <= 7) {
+          this.regFile.reg[register - 1] += value;
+        } else {
+          console.log("Error: Invalid register number!");
+        }
+        break;
+      }
+      case "sub": {
+        const register = parseInt(parts[1]);
+        const value = parseInt(parts[2]);
+
+        if (register >= 1 && register <= 7) {
+          this.regFile.reg[register - 1] -= value;
+        } else {
+          console.log("Error: Invalid register number!");
+        }
+        break;
+      }
+      case "put": {
+        const register = parseInt(parts[1]);
+        const value = parseInt(parts[2]);
+
+        if (register >= 1 && register <= 7) {
+          this.regFile.reg[register - 1] = value;
+        } else {
+          console.log("Error: Invalid register number!");
+        }
+        break;
+      }
+      case "view":
+        console.log("Registers:", this.regFile.reg);
+        console.log("Memory:", this.memory.data);
+        break;
+      case "save":
+        console.log("Saving the contents of memory and registers to a file...");
+        // Implement the saving logic here
+        break;
+      case "load":
+        console.log("Loading the contents of memory and registers from a file...");
+        // Implement the loading logic here
+        break;
+      case "run":
+        console.log("Executing the instructions in memory...");
+
+        while (this.programCounter < this.memory.data.length) {
+          const instruction = this.memory.load(this.programCounter);
+          this.executeInstruction(instruction);
+          this.programCounter++;
+        }
+
+        console.log("Program execution completed.");
+        break;
+      default:
+        console.log("Error: Invalid command!");
+        break;
+    }
+  }
+}
+
+// Example program
+const program = `add 1 10
+sub 2 5
+put 3 42
+put 4 78
+view
+save
+load
+run`;
+
+const cpu = new CPU();
+
+const commands = program.split("\n");
+
+for (let i = 0; i < commands.length; i++) {
+  cpu.executeCommand(commands[i]);
+}
